368cdbcf735a105ed432e9d92c8ddb6617dd9f2e
Sinan İci <sinan.ici@bupaacibadem.com.tr>
2024-12-01 14:38:09 +0300

    01122024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,58 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [FormsModule],
+  template: `
+    <h1>Todo App</h1>
+    @if (!isUpdateWorkFormActive) {
+    <div>
+      <label>Work</label>
+      <input [(ngModel)]="work" />
+      <button (click)="save()">Save</button>
+    </div>
+    } @else {
+    <div>
+      <label>UpdateWork</label>
+      <input [(ngModel)]="updatework" />
+      <button (click)="update()">Update</button>
+    </div>
+    }
+    <hr />
+    <div>
+      <ul>
+        @for(data of todos; track data){
+        <li>{{ data }}</li>
+        @if (!isUpdateWorkFormActive) {
+        <button (click)="delete($index)">Delete</button>
+        <button (click)="get($index)">Update</button>
+        } }
+      </ul>
+    </div>
+  `,
 })
 export class AppComponent {
-  title = 'todo-app';
+  work: string = '';
+  updatework: string = '';
+  updateIndex: number = 0;
+  todos: string[] = [];
+  isUpdateWorkFormActive: boolean = false;
+  save() {
+    this.todos.push(this.work);
+    this.work = '';
+  }
+  delete(index: number) {
+    this.todos.splice(index, 1);
+  }
+  get(index: number) {
+    this.updateIndex = index;
+    this.updatework = this.todos[index];
+    this.isUpdateWorkFormActive = true;
+  }
+  update() {
+    this.todos[this.updateIndex] = this.updatework;
+    this.isUpdateWorkFormActive = false;
+  }
 }
